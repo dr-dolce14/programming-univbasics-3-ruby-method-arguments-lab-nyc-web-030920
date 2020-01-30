3495bbf630641a2778096d61c62ab2f7c4697036
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-01-30 15:19:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,9 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_languate(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
